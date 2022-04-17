c415a18880f2347e4f3b488a75370b921b5b6ae7
Yang <yl4070@rit.edu>
2022-04-16 22:09:28 -0400

    add corr_rm, nzv

--- src/rdp_cli/cor_var.py
+++ src/rdp_cli/cor_var.py
@@ -10,6 +10,32 @@ import click
 
 
 
+@click.command()
+@click.argument("data")
+@click.option("--threshold", default = 0.8)
+def cor_rm(data, threshold):
+    df = load_data(data)
+    names = df.names
+    remove = []
+    for i, col in enumerate(names):
+        if i+1 > len(names)-1: break
+
+        cor_df = df[:, dt.corr(f[col], f[names[i+1:]])]
+        rhs = names[i+1:]
+        # break
+        for j, n in enumerate(rhs):
+            # print(j)
+            if cor_df[0, j] == None:
+                click.secho("Warning: some column may have zero variance!", fg = "yellow")
+                continue
+            elif cor_df[0, j] > threshold:
+                remove.append(n)
+
+    del df[:, remove]
+
+    df.to_csv(data)
+    click.echo("Success!")
+
 
 @click.command()
 @click.argument('data')
@@ -21,8 +47,11 @@ def nzv(data, threshold = 0):
     names = df.names
     remove = []
     for col in names:
-        if df[:, dt.sd(f[col])][0,0] < threshold:
-           remove.append(col) 
+        try:
+            if df[:, dt.sd(f[col])][0,0] < threshold:
+                remove.append(col) 
+        except:
+            click.secho("String columns are skipped!", fg = "yellow")
 
     del df[:, remove]
 
